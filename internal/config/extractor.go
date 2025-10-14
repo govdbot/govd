682c829f2e0e2c2d6b890cf4130b428ab8c2e788
stefanodvx <strsfn05s10c352s@studenti.unical.it>
2025-10-14 23:10:59 +0200

    Update extractor.go

--- internal/config/extractor.go
+++ internal/config/extractor.go
@@ -30,6 +30,29 @@ func loadExtractorConfigs() {
 		logger.L.Fatalf("failed parsing config file: %v", err)
 	}
 	maps.Copy(extractorConfigs, rawConfig)
+
+	validateConfig()
+}
+
+func validateConfig() {
+	for id, cfg := range extractorConfigs {
+		var active int
+		if cfg.Proxy != "" {
+			active++
+		}
+		if cfg.EdgeProxy != "" {
+			active++
+		}
+		if cfg.DisableProxy {
+			active++
+		}
+		if active > 1 {
+			logger.L.Fatalf("[%s] invalid config: cannot enable more than one proxy option at the same time", id)
+		}
+		if cfg.Instance != "" && id != "youtube" {
+			logger.L.Fatalf("[%s] invalid config: custom instance is only supported for youtube extractor", id)
+		}
+	}
 }
 
 func GetExtractorConfig(extractorID string) *ExtractorConfig {
