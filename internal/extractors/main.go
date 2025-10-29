431f18e37e6543d7b33148d6a317a679b4abeb98
stefanodvx <strsfn05s10c352s@studenti.unical.it>
2025-10-29 13:14:16 +0100

    enable reddit

--- internal/extractors/main.go
+++ internal/extractors/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/govdbot/govd/internal/extractors/instagram"
 	"github.com/govdbot/govd/internal/extractors/ninegag"
 	"github.com/govdbot/govd/internal/extractors/pinterest"
+	"github.com/govdbot/govd/internal/extractors/reddit"
 	"github.com/govdbot/govd/internal/extractors/soundcloud"
 	"github.com/govdbot/govd/internal/extractors/tiktok"
 	"github.com/govdbot/govd/internal/extractors/twitter"
@@ -25,4 +26,6 @@ var Extractors = []*models.Extractor{
 	youtube.Extractor,
 	pinterest.ShortExtractor,
 	pinterest.Extractor,
+	reddit.Extractor,
+	reddit.ShortExtractor,
 }
