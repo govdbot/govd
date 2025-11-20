7b5b97b5cf97c59c6c0daac501f34bd068d05c17
stefanodvx <strsfn05s10c352s@studenti.unical.it>
2025-11-20 19:17:37 +0100

    feat: add Vork muxer support and implement double pass remuxing (#29)

---
+++ internal/extractors/twitter/vork.go
@@ -0,0 +1,43 @@
+package twitter
+
+import (
+	"os"
+	"strings"
+
+	"github.com/abema/go-mp4"
+	"github.com/sunfish-shogi/bufseekio"
+)
+
+// vork is an internal muxer used by Twitter and needs
+// double muxing to be compatible with standard players.
+// learn more: https://github.com/govdbot/govd/issues/29
+func IsVorkMuxer(inputPath string) bool {
+	buf, err := os.Open(inputPath)
+	if err != nil {
+		return false
+	}
+	defer buf.Close()
+
+	r := bufseekio.NewReadSeeker(buf, 128*1024, 4)
+	path := mp4.BoxPath{
+		mp4.BoxTypeMoov(),
+		mp4.BoxTypeTrak(),
+		mp4.BoxTypeMdia(),
+		mp4.BoxTypeHdlr(),
+	}
+	boxes, err := mp4.ExtractBoxesWithPayload(r, nil, []mp4.BoxPath{path})
+	if err != nil {
+		panic(err)
+	}
+	for _, bi := range boxes {
+		hdlr, ok := bi.Payload.(*mp4.Hdlr)
+		if !ok {
+			continue
+		}
+		name := strings.TrimRight(hdlr.Name, "\x00") // null-terminated
+		if strings.Contains(name, "Twitter-vork") {
+			return true
+		}
+	}
+	return false
+}
--- internal/util/libav/remux.go
+++ internal/util/libav/remux.go
@@ -3,17 +3,24 @@ package libav
 import (
 	"os"
 
+	"github.com/govdbot/govd/internal/extractors/twitter"
 	"github.com/govdbot/govd/internal/logger"
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
 func RemuxFile(inputPath string, outputPath string) error {
+	isVork := twitter.IsVorkMuxer(inputPath)
+	if isVork {
+		return RemuxFileWithDoublePass(inputPath, outputPath)
+	}
+
 	logger.L.Debugf("remuxing file: %s", inputPath)
 
 	err := ffmpeg.Input(inputPath).
 		Output(outputPath, ffmpeg.KwArgs{
-			"c:v": "copy",
-			"c:a": "copy",
+			"map":      "0",
+			"c":        "copy",
+			"movflags": "+faststart",
 		}).
 		Silent(true).
 		OverWriteOutput().
@@ -23,5 +30,44 @@ func RemuxFile(inputPath string, outputPath string) error {
 		os.Remove(outputPath)
 		return err
 	}
+	return nil
+}
+
+func RemuxFileWithDoublePass(inputPath string, outputPath string) error {
+	logger.L.Debugf("remuxing file with double pass: %s", inputPath)
+
+	tempPath := inputPath + ".temp.mkv"
+	err := ffmpeg.Input(inputPath).
+		Output(tempPath, ffmpeg.KwArgs{
+			"map":      "0",
+			"c":        "copy",
+			"movflags": "+faststart",
+		}).
+		Silent(true).
+		OverWriteOutput().
+		Run()
+
+	if err != nil {
+		os.Remove(tempPath)
+		return err
+	}
+
+	err = ffmpeg.Input(tempPath).
+		Output(outputPath, ffmpeg.KwArgs{
+			"map":      "0",
+			"c":        "copy",
+			"movflags": "+faststart",
+		}).
+		Silent(true).
+		OverWriteOutput().
+		Run()
+
+	os.Remove(tempPath)
+
+	if err != nil {
+		os.Remove(outputPath)
+		return err
+	}
+
 	return nil
 }
