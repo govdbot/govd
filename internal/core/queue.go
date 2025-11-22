7c79677ea163e2c3629906c44ecc131a308a8fda
stefanodvx <strsfn05s10c352s@studenti.unical.it>
2025-11-22 02:48:13 +0100

    fix: add type assertion checks in acquireQueue and releaseQueue functions

--- internal/core/queue.go
+++ internal/core/queue.go
@@ -8,13 +8,19 @@ var queueLock sync.Map
 
 func acquireQueue(key string) {
 	value, _ := queueLock.LoadOrStore(key, &sync.Mutex{})
-	mu := value.(*sync.Mutex)
+	mu, ok := value.(*sync.Mutex)
+	if !ok {
+		return
+	}
 	mu.Lock()
 }
 
 func releaseQueue(key string) {
 	if value, ok := queueLock.Load(key); ok {
-		mu := value.(*sync.Mutex)
+		mu, ok := value.(*sync.Mutex)
+		if !ok {
+			return
+		}
 		mu.Unlock()
 	}
 }
